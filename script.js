d8f7a95ab74adee885860061273efc3f71958886
Jer <CodeJ3r@gmail.com>
2025-02-20 15:16:18 -0400

    Update script.js-Jeremy fixed it!
    Corrected errors in the java script file.

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-student_name = Nadia;
-studentAge == "20";
-isEnrolled = True; 
+/*
+Jeremy Paruch
+w0222971
+February 20, 2025
+Nadia Gouda
+ClientSide Programming
 
-document.write("<h2>Student Info</h2>")
-document.write("Name: " + studentName + <br>); 
-document.write("Age: " + student_age); 
-document.write("Enrolled: " + isEnrolled) 
+*/
 
-let ageString = String(studentAge)
-document.write("Age Type: " typeof ageString); 
+student_name = "Nadia"; // added quotations around nadia
+studentAge =20; // removed quotations from 20
+isEnrolled = true; // removed captial T
 
-num1 = prompt("Enter first number");
-num2 = prompt("Enter second number");
-total = num1 + num2; 
+document.write("<h2>Student Info</h2>"); //semi colon for good practice
+document.write("Name: " + student_name + "<br>"); // quotations around br, cahnged to student_name
+document.write("Age: " + studentAge); // studentAge
+document.write("Enrolled: " + isEnrolled); // SEMI COLON
 
-document.write("Sum: " + total)
+let ageString = String(studentAge); // SEMI COLON
+document.write("Age Type: " + typeof (ageString)); //ADDED + AND brackets around agestring
 
-userAge = prompt("Enter your age");
-if (userAge = 18) { 
+num1 = Number(prompt("Enter first number")); // added type casting to turn the prompt into a integer
+num2 = Number(prompt("Enter second number"));// added type casting to turn the prompt into a integer
+document.write("Sum: ", + num1 + num2); // added comma after Sum" , deleted original 15 was redundant
+
+userAge = Number(prompt("Enter your age")); // typecasting again
+if (userAge >=18) {                         // changed to greater than or equal to
     document.write("You can vote!"); 
-} else (userAge < 18) { 
+} else { // just need else here
     document.write("Sorry, you can't vote."); 
 }
 
-for (i = 1; i < 10; i++) 
-    document.write(i)
+for (i = 1; i < 10; i++) { // Needs some curly brackets in there or it wont work the way you want it to...for good habits
+    document.write(i); 
+}
+let num =0; //adding num variable
+while (num < 10 ) { // added curly brackets
 
-while num < 10 
-    num = prompt("Enter a number greater than 10")
+    num = Number(prompt("Enter a number greater than 10")); // added ;
+
+}
